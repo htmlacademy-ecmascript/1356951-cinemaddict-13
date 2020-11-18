b6ab3e9ce0eb16cf5952303b45adcb0f3cbf2620
Victoria <morriface@mail.ru>
2020-11-18 13:48:27 +0300

    Вставка user  в код

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import {createUserTemplate} from "./view/user.js";
+
+
+
+
 const render = (parent, element) => {
   const newFragment = document.createDocumentFragment();
   parent.innerHTML = ``;
   newFragment.appendChild(element);
   parent.appendChild(newFragment);
 };
+
+const headerElement = document.querySelector(`.header`);
+render(headerElement, createUserTemplate());
