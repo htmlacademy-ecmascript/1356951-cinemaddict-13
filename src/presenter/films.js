2406aac420eca6bf425cba2f1f6fef7efba025c4
Victoria <morriface@mail.ru>
2020-12-17 09:15:55 +0300

    Подготавливаю рендер-функции к модели

--- src/presenter/films.js
+++ src/presenter/films.js
@@ -1,4 +1,4 @@
-import {remove, render, RenderPosition, updateItem} from "../utils.js";
+import {remove, render, RenderPosition/* , updateItem*/} from "../utils.js";
 import Button from "../view/button.js";
 import Film from "../presenter/film.js";
 import dayjs from "dayjs";
@@ -16,10 +16,11 @@ import {SortType} from "../const.js";
 const TOP_CARD_FILM_QUANTITY = 2;
 const COMMENTED_CARD_FILM_QUANTITY = 2;
 const FILM_COUNT_PER_STEP = 5;
-let cardFilmQuantity = 5;
+// let cardFilmQuantity = 5;
 
 export default class Films {
   constructor() {
+    this._renderedFilmsCount = FILM_COUNT_PER_STEP;
     this._sortComponent = new Sort();
     this._filmContainer = new FilmContainer();
     this._filmList = new FilmList();
@@ -33,7 +34,7 @@ export default class Films {
     this._filmTopPresenter = {};
     this._filmCommentedPresenter = {};
     // this._popup = new Popup();
-    this._renderedFilmCount = null;
+    // this._renderedFilmCount = null;
     this._hadleFilmChange = this._hadleFilmChange.bind(this);
     this._filmListContainerTop = new FilmListContainer();
     this._filmListContainerCommented = new FilmListContainer();
@@ -44,9 +45,9 @@ export default class Films {
 
   init(films = []) {
     // Можешь объяснить почему в this._films меняется порядок фильмов, но при отрисовке действует старый порядок?
-    this._films = films.getFilms().slice();
-    this._sourseFilms = films.getFilms().slice();
-    if (this._films.length === 0) {
+    this._films = films;
+    // this._sourseFilms = films.getFilms().slice();
+    if (this._films.getFilms().slice().length === 0) {
       this._renderEmptyFilmsList();
     } else {
       this._renderSort();
@@ -62,47 +63,57 @@ export default class Films {
     // сортировка
   }
 
-  _renderSortedFilms(sortType) {
+  _getFilms() { // renderSortedFilms
     // сортировка
-    switch (sortType) {
+    switch (this._currentSortType) {
       case SortType.DATE:
-        this._films.sort(function (a, b) {
+        this._films.getFilms().slice().sort(function (a, b) {
           return dayjs(b.releaseDate).diff(dayjs(a.releaseDate));
         });
         break;
       case SortType.RATING:
-        this._films.sort(function (a, b) {
+        this._films.getFilms().slice().sort(function (a, b) {
           return b.rating - a.rating;
         }).slice();
         break;
-      default:
+      // default:
         // 3. А когда пользователь захочет "вернуть всё, как было",
         // мы просто запишем в _sourseFilms исходный массив
-        this._films = this._sourseFilms.slice();
+       // this._films = this._sourseFilms.slice();
     }
-
-    this._currentSortType = sortType;
+    return this._films.getFilms();
+    //  this._currentSortType = sortType;
   }
 
   _handleSortTypeChange(sortType) {
     if (this._currentSortType === sortType) {
       return;
     }
-    this._renderSortedFilms(sortType);
+    this._currentSortType = sortType;
+    // сортировка
+    // this._getFilms(sortType);
     // очищаем список
     this._clearFilms();
     // рендерим новый
     this._renderFilmsList();
   }
 
+  _renderFilms(films) {
+    films.forEach((film) => this._renderFilm(this._filmListContainer.getElement(), film));
+  }
+
   _renderFilmsList() {
     // отрисовка фильмов
-    if (this._films.length < cardFilmQuantity) {
-      cardFilmQuantity = this._films.length;
-    }
-    for (let i = 0; i < cardFilmQuantity; i++) {
-      this._renderFilm(this._filmListContainer.getElement(), this._films[i]);
-    }
+    /* if (this._getFilms.slice().length < cardFilmQuantity) {
+      cardFilmQuantity = this._getFilms.length;
+    }*/
+    const filmCount = this._getFilms().length;
+
+    const filmsToRender = this._getFilms().slice(0, Math.min(filmCount, FILM_COUNT_PER_STEP));
+    this._renderFilms(filmsToRender);
+    /* for (let i = 0; i < cardFilmQuantity; i++) {
+      this._renderFilm(this._filmListContainer.getElement(), this._getFilms[i]);
+    }*/
     if (this._filmContainer.getElement().querySelector(`.films-list__show-more`) === null) {
       this._renderButton();
     }
@@ -143,7 +154,7 @@ export default class Films {
       .values(this._filmPresenter)
       .forEach((presenter) => presenter.destroy());
     this._filmPresenter = {};
-    this._renderedFilmCount = FILM_COUNT_PER_STEP;
+    this._renderedFilmsCount = FILM_COUNT_PER_STEP;
     remove(this._button);
   }
 
@@ -153,7 +164,8 @@ export default class Films {
   }
 
   _hadleFilmChange(updateFilm) {
-    this._films = updateItem(this._films, updateFilm);
+    // Обновление модели
+    // this._films = updateItem(this._films, updateFilm);
     if (this._filmPresenter[updateFilm.id]) {
       this._filmPresenter[updateFilm.id].filmInit(updateFilm);
     }
@@ -167,18 +179,28 @@ export default class Films {
 
   _renderButton() {
     // отрисовка кнопки
-    if (this._films.length > FILM_COUNT_PER_STEP) {
+    const filmCount = this._getFilms().length;
+    const filmsToRender = this._getFilms().slice(this._renderedFilmsCount, Math.min(this._renderedFilmsCount + FILM_COUNT_PER_STEP, filmCount));
+
+    if (filmCount > FILM_COUNT_PER_STEP) {
       // Добавляем кнопку
       render(this._filmList, this._button, RenderPosition.BEFOREEND);
       this._button.setButtonClickListeners(() => {
-        this._films
+        filmsToRender.forEach((film) => this._renderFilm(this._filmListContainer.getElement(), film));
+
+        this._renderedFilmsCount += FILM_COUNT_PER_STEP;
+        this._button.getElement().remove();
+        this._button.removeElement();
+        // remove(this._button);
+
+        /* this._getFilms
         .slice(this._renderedFilmCount, this._renderedFilmCount + FILM_COUNT_PER_STEP)
         .forEach((film) => this._renderFilm(this._filmListContainer.getElement(), film));
         this._renderedFilmCount += FILM_COUNT_PER_STEP;
-        if (this._renderedFilmCount >= this._films.length) {
+        if (this._renderedFilmCount >= this._getFilms.slice().length) {
           this._button.getElement().remove();
           this._button.removeElement();
-        }
+        }*/
       });
     }
     if (this._filmContainer.getElement().querySelector(`.films-list--extra`) === null) {
@@ -191,26 +213,28 @@ export default class Films {
     // отрисовка топ фильмов
     render(this._filmContainer, this._filmListTop, RenderPosition.BEFOREEND);
     render(this._filmListTop, this._filmListContainerTop, RenderPosition.BEFOREEND);
-    let topCardQuantity = this._films.length > 1 ? TOP_CARD_FILM_QUANTITY : this._films.length;
-    this._topFilms = this._films.sort(function (a, b) {
+    let topCardQuantity = this._getFilms().slice().length > 1 ? TOP_CARD_FILM_QUANTITY : this._getFilms().slice().length;
+    this._topFilms = this._getFilms().slice().sort(function (a, b) {
       return b.rating - a.rating;
     }).slice(0, topCardQuantity);
-    for (let i = 0; i < topCardQuantity; i++) {
+    this._topFilms.forEach((film) => this._renderTopFilm(this._filmListContainerTop.getElement(), film));
+    /* for (let i = 0; i < topCardQuantity; i++) {
       this._renderTopFilm(this._filmListContainerTop.getElement(), this._topFilms[i]);
-    }
+    }*/
   }
 
   _renderCommentedFilms() {
   // отрисовка комментируемых фильмов
     render(this._filmContainer, this._filmListCommented, RenderPosition.BEFOREEND);
     render(this._filmListCommented, this._filmListContainerCommented, RenderPosition.BEFOREEND);
-    let commentedCardQuantity = this._films.length > 1 ? COMMENTED_CARD_FILM_QUANTITY : 1;
-    const commentedFilms = this._films.sort(function (a, b) {
+    let commentedCardQuantity = this._getFilms().length > 1 ? COMMENTED_CARD_FILM_QUANTITY : 1;
+    const commentedFilms = this._getFilms().slice().sort(function (a, b) {
       return b.comments.length - a.comments.length;
-    });
-    for (let i = 0; i < commentedCardQuantity; i++) {
+    }).slice(0, commentedCardQuantity);
+    commentedFilms.forEach((film) => this._renderCommentedFilm(this._filmListContainerCommented.getElement(), film));
+    /* for (let i = 0; i < commentedCardQuantity; i++) {
       this._renderCommentedFilm(this._filmListContainerCommented.getElement(), commentedFilms[i]);
-    }
+    }*/
   }
   _renderSort() {
     render(mainElement, this._sortComponent, RenderPosition.BEFOREEND);
