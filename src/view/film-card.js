7c417fe8ebc3c7ef28cb3fbd20cd1844bfe6c4c7
Victoria <morriface@mail.ru>
2020-12-30 13:57:15 +0300

    Отрисовка в карточке фильма только года, вместо даты. Перевела минуты в часы и минуты в карточке фильма.

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -20,13 +20,24 @@ const createFilmCardTemplate = (film) => {
     return activeClass;
   };
 
+  const getYear = () => {
+    const onlyYear = year.getFullYear();
+    return onlyYear;
+  };
+
+  const getTimeFromMins = (mins) => {
+    let hours = Math.trunc(mins / 60);
+    let minutes = (mins % 60) < 10 ? `0` + `${(mins % 60)}` : mins % 60;
+    return hours + `h. ` + minutes + `m.`;
+  };
+
   return (
     `<article class="film-card">
       <h3 class="film-card__title">${filmName}</h3>
       <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">${year}</span>
-        <span class="film-card__duration">${duration}</span>
+        <span class="film-card__year">${getYear()}</span>
+        <span class="film-card__duration">${getTimeFromMins(duration)}</span>
         <span class="film-card__genre">${genre[0]}</span>
       </p>
       <img src=${poster} alt="" class="film-card__poster">
